04d8dcd8db8f542e54c1bd98869ac0b7753c36b7
Nyuks85 <jorgejg@iMac-de-Jorge.local>
2019-04-07 11:30:37 +0200

    Input Text
    Input text first

---
+++ app.js
@@ -0,0 +1,7 @@
+function AddText()
+{
+    var InputLeft = document.getElementById("InputLeft");
+    var InputRight = document.getElementById("InputRight");
+
+    InputRight.value = InputLeft.value;
+}
